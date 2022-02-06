3ed6da902db6d2f6fa4a5ac67ee31044073ec3e1
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-02-05 19:10:40 -0500

    templates script

---
+++ templates/script_template.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import dvrk
+from pathlib import Path
+from kincalib.utils.Logger import Logger
+from kincalib.utils.SavingUtilities import save_without_overwritting
+import argparse
+import sys
+from pathlib import Path
+import time
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+
+    main()
