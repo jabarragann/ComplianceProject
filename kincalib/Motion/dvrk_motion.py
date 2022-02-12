87dabf4636102c04d90fcce49a504de5b21c501b
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-02-12 13:29:29 -0500

    dvrk motions completed

--- kincalib/Motion/dvrk_motion.py
+++ kincalib/Motion/dvrk_motion.py
@@ -25,7 +25,84 @@ class DvrkMotions:
         return trajectory
 
     @staticmethod
-    def pitch_axis_identification(
+    def generate_roll_motion(steps: int = 22) -> np.ndarray:
+        min_pitch = -0.6
+        max_pitch = 0.2
+        trajectory = np.linspace(min_pitch, max_pitch, num=steps)
+        return trajectory
+
+    @staticmethod
+    def create_df_with_measurements(
+        ftk_handler, expected_markers, idx, q4, q5, df_cols
+    ) -> pd.DataFrame:
+
+        # Read atracsys data for 1 second - fiducials data
+        # Sensor_vals will have several measurements of the static fiducials
+        mean_frame, mean_value = ftk_handler.obtain_processed_measurement(
+            expected_markers, t=500, sample_time=15
+        )
+        df_vals = pd.DataFrame(columns=df_cols)
+
+        # Add fiducials to dataframe
+        # df columns: ["step","q4" "q5", "m_t", "m_id", "px", "py", "pz", "qx", "qy", "qz", "qw"]
+        if mean_value is not None:
+            log.warning("No fiducial found")
+            for mid, k in enumerate(range(mean_value.shape[0])):
+                # fmt:off
+                d = [ idx, q4, q5, "f", mid, mean_value[k, 0], mean_value[k, 1], mean_value[k, 2], 0.0, 0.0, 0.0, 1 ]
+                # fmt:on
+                d = np.array(d).reshape((1, 11))
+                new_pt = pd.DataFrame(d, columns=df_cols)
+                df_vals = df_vals.append(new_pt)
+        # Add marker pose to dataframe
+        if mean_frame is not None:
+            log.warning("No markers found")
+            p = list(mean_frame.p)
+            q = mean_frame.M.GetQuaternion()
+            d = [idx, q4, q5, "m", 112, p[0], p[1], p[2], q[0], q[1], q[2], q[3]]
+            d = np.array(d).reshape((1, 11))
+            new_pt = pd.DataFrame(d, columns=df_cols)
+            df_vals = df_vals.append(new_pt)
+        if mean_frame is None and mean_value is None:
+            return None
+        else:
+            return df_vals
+
+    # ------------------------------------------------------------
+    # DVRK motions
+    # - pitch_roll_independent_motion
+    # - pitch_roll_together_motion
+    # - pitch_motion
+    # - roll_motion
+    # ------------------------------------------------------------
+    @staticmethod
+    def pitch_roll_independent_motion(
+        init_jp, psm_handler, log, expected_markers=4, save: bool = False, filename: Path = None
+    ):
+        """Move each axis independently. Save the measurements from each movement in different df.
+
+        Args:
+            init_jp ([type]): [description]
+            psm_handler ([type]): [description]
+            log ([type]): [description]
+            expected_markers (int, optional): [description]. Defaults to 4.
+            save (bool, optional): [description]. Defaults to False.
+            filename (Path, optional): [description]. Defaults to None.
+
+        """
+
+        pitch_trajectory = DvrkMotions.generate_pitch_motion()
+        roll_trajectory = DvrkMotions.generate_roll_motion()
+
+        pitch_file = filename.parent / (filename.with_suffix("").name + "_pitch.txt")
+        roll_file = filename.parent / (filename.with_suffix("").name + "_roll.txt")
+        # fmt:off
+        DvrkMotions.pitch_motion( init_jp, psm_handler, log, expected_markers, save, pitch_file, trajectory=pitch_trajectory)
+        DvrkMotions.roll_motion(  init_jp, psm_handler, log, expected_markers, save, roll_file, trajectory=roll_trajectory)
+        # fmt:on
+
+    @staticmethod
+    def pitch_roll_together_motion(
         init_jp, psm_handler, log, expected_markers=4, save: bool = False, filename: Path = None
     ):
         """Identify pitch axis by moving outer roll (shaft movement) and wrist pitch joint.
@@ -52,7 +129,7 @@ class DvrkMotions:
         # Start trajectory
         counter = 0
         for q4, q5 in track(
-            product(yaw_trajectory, pitch_trajectory),
+            product(roll_trajectory, pitch_trajectory),
             total=total,
             description="-- Trajectory Progress -- ",
         ):
@@ -66,90 +143,106 @@ class DvrkMotions:
             time.sleep(0.5)
 
             if save:
-                # Read atracsys data for 1 second - fiducials data
-                # Sensor_vals will have several measurements of the static fiducials
-                mean_frame, mean_value = ftk_handler.obtain_processed_measurement(
-                    expected_markers, t=500, sample_time=15
+                new_pt = DvrkMotions.create_df_with_measurements(
+                    ftk_handler, expected_markers, counter, q4, q5, df_cols
                 )
-
-                if mean_frame is not None and mean_value is not None:
-                    # Add fiducials to dataframe
-                    # df columns: ["step","q4" "q5", "m_t", "m_id", "px", "py", "pz", "qx", "qy", "qz", "qw"]
-                    for mid, k in enumerate(range(mean_value.shape[0])):
-                        # fmt:off
-                        d = [ counter,q4, q5, "f", mid, mean_value[k, 0], mean_value[k, 1], mean_value[k, 2], 0.0, 0.0, 0.0, 1 ]
-                        # fmt:on
-                        d = np.array(d).reshape((1, 12))
-                        new_pt = pd.DataFrame(d, columns=df_cols)
-                        df_vals = df_vals.append(new_pt)
-                    # Add marker pose to dataframe
-                    p = list(mean_frame.p)
-                    q = mean_frame.M.GetQuaternion()
-                    d = [counter, q4, q5, "m", 112, p[0], p[1], p[2], q[0], q[1], q[2], q[3]]
-                    d = np.array(d).reshape((1, 12))
-                    new_pt = pd.DataFrame(d, columns=df_cols)
+                if df_vals is not None:
                     df_vals = df_vals.append(new_pt)
-                else:
-                    if mean_frame is None:
-                        log.warning("no markers found")
-                    elif mean_value:
-                        log.warning("No fiducials found")
+
         # Save experiment
         log.debug(df_vals.head())
         if save:
             save_without_overwritting(df_vals, Path(filename))
             # df_vals.to_csv("./data/02_pitch_experiment/pitch_exp03.txt", index=None)
 
-    def pitch_experiment(
-        init_jp, psm_handler, log, expected_markers=4, save: bool = True, filename: Path = None
+    @staticmethod
+    def roll_motion(
+        init_jp,
+        psm_handler,
+        log,
+        expected_markers=4,
+        save: bool = True,
+        filename: Path = None,
+        trajectory=None,
     ):
 
         ftk_handler = ftk_500("custom_marker_112")
-        trajectory = DvrkMotions.generate_pitch_motion()
+        if trajectory is None:
+            trajectory = DvrkMotions.generate_pitch_motion()
 
-        df_cols = ["step", "q5", "m_t", "m_id", "px", "py", "pz", "qx", "qy", "qz", "qw"]
+        df_cols = ["step", "q4", "q5", "m_t", "m_id", "px", "py", "pz", "qx", "qy", "qz", "qw"]
         df_vals = pd.DataFrame(columns=df_cols)
 
         # Move to initial position
         psm_handler.move_jp(init_jp).wait()
         time.sleep(1)
-        # Move pitch joint from min_pitch to max_pitch
-        for idx, q5 in enumerate(trajectory):
-            log.info(f"q5-{idx}@{q5*180/np.pi:0.2f}(deg)@{q5:0.2f}(rad)")
-            init_jp[4] = q5  ##Only move pitch axis (joint 5) - joint in index 4
+
+        q4 = init_jp[3]
+        q5 = init_jp[4]
+        # Move roll joint from min_roll to max_roll
+        for idx, q4 in enumerate(trajectory):
+            log.info(f"q4-{idx}@{q4*180/np.pi:0.2f}(deg)@{q4:0.2f}(rad)")
+            # Move only q4 and q5
+            init_jp[3] = q4
+            init_jp[4] = q5
+            init_jp[5] = 0.0
             psm_handler.move_jp(init_jp).wait()
             time.sleep(0.5)
 
-            # Read atracsys data for 1 second - fiducials data
-            # Sensor_vals will have several measurements of the static fiducials
-            mean_frame, mean_value = ftk_handler.obtain_processed_measurement(
-                expected_markers, t=500, sample_time=15
-            )
-
-            if mean_frame is not None and mean_value is not None:
-                # Add fiducials to dataframe
-                # df columns: ["step", "q5", "m_t", "m_id", "px", "py", "pz", "qx", "qy", "qz", "qw"]
-                for mid, k in enumerate(range(mean_value.shape[0])):
-                    # fmt:off
-                    d = [ idx, q5, "f", mid, mean_value[k, 0], mean_value[k, 1], mean_value[k, 2], 0.0, 0.0, 0.0, 1 ]
-                    # fmt:on
-                    d = np.array(d).reshape((1, 11))
-                    new_pt = pd.DataFrame(d, columns=df_cols)
+            if save:
+                new_pt = DvrkMotions.create_df_with_measurements(
+                    ftk_handler, expected_markers, idx, q4, q5, df_cols
+                )
+                if df_vals is not None:
                     df_vals = df_vals.append(new_pt)
-                # Add marker pose to dataframe
-                p = list(mean_frame.p)
-                q = mean_frame.M.GetQuaternion()
-                d = [idx, q5, "m", 112, p[0], p[1], p[2], q[0], q[1], q[2], q[3]]
-                d = np.array(d).reshape((1, 11))
-                new_pt = pd.DataFrame(d, columns=df_cols)
-                df_vals = df_vals.append(new_pt)
-            else:
-                log.warning("No markers found")
         # Save experiment
         log.debug(df_vals.head())
         if save:
             save_without_overwritting(df_vals, Path(filename))
-            # df_vals.to_csv("./data/02_pitch_experiment/pitch_exp03.txt", index=None)
+
+    @staticmethod
+    def pitch_motion(
+        init_jp,
+        psm_handler,
+        log,
+        expected_markers=4,
+        save: bool = True,
+        filename: Path = None,
+        trajectory=None,
+    ):
+
+        ftk_handler = ftk_500("custom_marker_112")
+        if trajectory is None:
+            trajectory = DvrkMotions.generate_pitch_motion()
+        df_cols = ["step", "q4", "q5", "m_t", "m_id", "px", "py", "pz", "qx", "qy", "qz", "qw"]
+        df_vals = pd.DataFrame(columns=df_cols)
+
+        # Move to initial position
+        psm_handler.move_jp(init_jp).wait()
+        time.sleep(1)
+
+        q4 = init_jp[3]
+        q5 = init_jp[4]
+        # Move pitch joint from min_pitch to max_pitch
+        for idx, q5 in enumerate(trajectory):
+            log.info(f"q5-{idx}@{q5*180/np.pi:0.2f}(deg)@{q5:0.2f}(rad)")
+            # Move only q4 and q5
+            init_jp[3] = q4
+            init_jp[4] = q5
+            init_jp[5] = 0.0
+            psm_handler.move_jp(init_jp).wait()
+            time.sleep(0.5)
+
+            if save:
+                new_pt = DvrkMotions.create_df_with_measurements(
+                    ftk_handler, expected_markers, idx, q4, q5, df_cols
+                )
+                if df_vals is not None:
+                    df_vals = df_vals.append(new_pt)
+        # Save experiment
+        log.debug(df_vals.head())
+        if save:
+            save_without_overwritting(df_vals, Path(filename))
 
     @staticmethod
     def pitch_experiment_single_marker(init_pos, psm_handler, log, expected_markers=4, save=False):
@@ -207,25 +300,30 @@ if __name__ == "__main__":
     # ------------------------------------------------------------
     # Pitch identification exp 01
     # ------------------------------------------------------------
-    filename = Path("./data/02_pitch_experiment/d05-pitch-yaw_exp10.txt")
     # init_jp = np.array([0.1364, 0.2608, 0.1314, -1.4911, 0.216, 0.1738])  # 06
     init_jp = psm_handler.measured_jp()
+    log.info(f"Joints current state \n {init_jp}")
     input("Press enter to start motion")
-    psm_handler.move_jp(init_jp).wait()
     time.sleep(0.5)
-
-    # jp = psm_handler.measured_jp()
-    # log.info(f"Joints current state \n {jp}")
-
-    # # Move wrist pitch axis of the robot
-    # DvrkMotions.pitch_experiment(
-    #     init_jp, psm_handler=psm_handler, expected_markers=4, log=log, save=True, filename=filename
-    # )
 
     # ------------------------------------------------------------
     # Pitch identification exp 02
     # ------------------------------------------------------------
+    filename = Path("./data/02_pitch_experiment/d05-pitch-yaw_exp10.txt")
 
-    DvrkMotions.pitch_axis_identification(
+    # Only uncomment one of the motions at the same time
+    DvrkMotions.pitch_roll_together_motion(
         init_jp, psm_handler=psm_handler, expected_markers=4, log=log, save=True, filename=filename
     )
+
+    # DvrkMotions.pitch_roll_independent_motion(
+    #     init_jp, psm_handler=psm_handler, expected_markers=4, log=log, save=True, filename=filename
+    # )
+
+    # DvrkMotions.pitch_motion(
+    #     init_jp, psm_handler=psm_handler, expected_markers=4, log=log, save=True, filename=filename
+    # )
+
+    # DvrkMotions.roll_motion(
+    #     init_jp, psm_handler=psm_handler, expected_markers=4, log=log, save=True, filename=filename
+    # )
