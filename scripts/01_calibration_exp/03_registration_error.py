67cc69826cb084f4746522abb3a3cfface3017cc
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-02-23 23:08:37 -0500

    03_registration_scripts

--- scripts/01_calibration_exp/03_registration_error.py
+++ scripts/01_calibration_exp/03_registration_error.py
@@ -25,116 +25,111 @@ from kincalib.utils.Frame import Frame
 from kincalib.utils.Logger import Logger
 from kincalib.utils.SavingUtilities import save_without_overwritting
 from kincalib.utils.RosbagUtils import RosbagUtils
-from kincalib.utils.ExperimentUtils import separate_markerandfiducial, calculate_midpoints 
-from kincalib.geometry import Line3D,Circle3D, Triangle3D
+from kincalib.utils.ExperimentUtils import separate_markerandfiducial, calculate_midpoints
+from kincalib.geometry import Line3D, Circle3D, Triangle3D
 
 np.set_printoptions(precision=4, suppress=True, sign=" ")
 
 
 log = Logger("registration").log
 
+
 def calculate_registration():
-    #------------------------------------------------------------
-    # Setup 
-    #------------------------------------------------------------
-    parser = argparse.ArgumentParser()
-    #fmt:off
-    parser.add_argument( "-r", "--root", type=str, default="./data/03_replay_trajectory/d04-rec-01", 
-                         help="root dir") 
-    parser.add_argument( "-l", "--log", type=str, default="DEBUG", 
-                         help="log level") #fmt:on
-    args = parser.parse_args()
+    # ------------------------------------------------------------
+    # Setup
+    # ------------------------------------------------------------
+
     filename = Path("scripts/01_calibration_exp/results")
     root = Path(args.root)
-    filename = filename/(root.name+"_registration_data.txt")
+    filename = filename / (root.name + "_registration_data.txt")
     df = pd.read_csv(filename)
 
-    #Choose entries were the area is lower is than 6mm
-    df = df.loc[df['area']<10]
-    robot_p = df[['rpx','rpy','rpz']].to_numpy().T
-    pitch_p = df[['tpx','tpy','tpz']].to_numpy().T
+    # Choose entries were the area is lower is than 6mm
+    df = df.loc[df["area"] < 10]
+    robot_p = df[["rpx", "rpy", "rpz"]].to_numpy().T
+    pitch_p = df[["tpx", "tpy", "tpz"]].to_numpy().T
     log.info(f"Points used for the registration {df.shape}")
 
-    trans = Frame.find_transformation_direct(robot_p,pitch_p)
-    error = Frame.evaluation(robot_p,pitch_p,trans)
+    trans = Frame.find_transformation_direct(robot_p, pitch_p)
+    error = Frame.evaluation(robot_p, pitch_p, trans)
 
     log.info(f"transformation between robot and tracker\n{trans}")
     log.info(f"mean square error: {1000*error:0.05f}")
 
-def pitch_orig_in_robot(robot_df:pd.DataFrame, service_name:str="/PSM1/local/query_cp")->pd.DataFrame:
-    #------------------------------------------------------------
-    # Connect to DVRK fk service 
-    #------------------------------------------------------------
+
+def pitch_orig_in_robot(
+    robot_df: pd.DataFrame, service_name: str = "/PSM1/local/query_cp"
+) -> pd.DataFrame:
+    # ------------------------------------------------------------
+    # Connect to DVRK fk service
+    # ------------------------------------------------------------
     rospy.wait_for_service(service_name, timeout=1)
     kinematics_service = rospy.ServiceProxy(service_name, QueryForwardKinematics)
     log.info("connected to {:} ...".format(service_name))
 
-    #------------------------------------------------------------
-    # Itereate over date and calculate pitch origin in robot coordinates 
-    #------------------------------------------------------------
-    cols = ["step","rpx","rpy","rpz"]
-    df_results = pd.DataFrame(columns=cols) 
+    # ------------------------------------------------------------
+    # Itereate over date and calculate pitch origin in robot coordinates
+    # ------------------------------------------------------------
+    cols = ["step", "rpx", "rpy", "rpz"]
+    df_results = pd.DataFrame(columns=cols)
     for idx in range(robot_df.shape[0]):
         joints = JointState()
-        joints.position = robot_df.iloc[idx][['q1','q2','q3','q4']].to_list()
+        joints.position = robot_df.iloc[idx][["q1", "q2", "q3", "q4"]].to_list()
         msg = kinematics_service(joints)
         msg = pm.fromMsg(msg.cp.pose).p
-        data = [robot_df.iloc[idx]['step']] + list(msg)
-        data = np.array(data).reshape((1,-1))
+        data = [robot_df.iloc[idx]["step"]] + list(msg)
+        data = np.array(data).reshape((1, -1))
         new_df = pd.DataFrame(data, columns=cols)
         df_results = df_results.append(new_df)
 
     return df_results
 
-def pitch_orig_in_tracker(root:Path):
-    def load_files(root:Path):
-        dict_files = defaultdict(dict) #Use step as keys. Each entry has a pitch and roll file.
-        for f in (root/"pitch_roll_mov").glob("*"):
-            step = int(re.findall("step[0-9]+", f.name)[0][4:]) #NOT very reliable
-            if "pitch" in f.name :
-                dict_files[step]['pitch'] = pd.read_csv(f)
+
+def pitch_orig_in_tracker(root: Path):
+    def load_files(root: Path):
+        dict_files = defaultdict(dict)  # Use step as keys. Each entry has a pitch and roll file.
+        for f in (root / "pitch_roll_mov").glob("*"):
+            step = int(re.findall("step[0-9]+", f.name)[0][4:])  # NOT very reliable
+            if "pitch" in f.name:
+                dict_files[step]["pitch"] = pd.read_csv(f)
             elif "roll" in f.name:
-                dict_files[step]['roll'] = pd.read_csv(f)
+                dict_files[step]["roll"] = pd.read_csv(f)
 
-        return dict_files 
+        return dict_files
 
-    #------------------------------------------------------------
-    # Itereate over data and calculate pitch origin in tracker coordinates 
-    #------------------------------------------------------------
-    #Final df
-    cols = ["step","area","tpx","tpy","tpz"]
-    df_results = pd.DataFrame(columns=cols) 
+    # ------------------------------------------------------------
+    # Itereate over data and calculate pitch origin in tracker coordinates
+    # ------------------------------------------------------------
+    # Final df
+    cols = ["step", "area", "tpx", "tpy", "tpz"]
+    df_results = pd.DataFrame(columns=cols)
 
-    dict_files = load_files(root) #Use the step as the dictionary key
+    dict_files = load_files(root)  # Use the step as the dictionary key
     keys = sorted(list(dict_files.keys()))
 
     for k in track(keys, "Computing pitch origin in tracker coordinates"):
-        step = k 
-        m1,m2,m3 = calculate_midpoints(dict_files[k]['roll'],dict_files[k]['pitch'])
-        triangle = Triangle3D([m1,m2,m3])
-        #Scale sides and area to milimiters
+        if len(list(dict_files[k].keys())) < 2:
+            log.warning(f"files for step {k} are not available")
+            continue
+        step = k
+        m1, m2, m3 = calculate_midpoints(dict_files[k]["roll"], dict_files[k]["pitch"])
+        triangle = Triangle3D([m1, m2, m3])
+        # Scale sides and area to milimiters
         area = triangle.calculate_area(scale=1000)
         center = triangle.calculate_centroid()
-        #Add new entry to df
-        data = [step,area] + list(center)
-        data = np.array(data).reshape((1,-1))
+        # Add new entry to df
+        data = [step, area] + list(center)
+        data = np.array(data).reshape((1, -1))
         new_df = pd.DataFrame(data, columns=cols)
         df_results = df_results.append(new_df)
-    
+
     return df_results
 
 
 def main():
-    #------------------------------------------------------------
-    # Setup 
-    #------------------------------------------------------------
-    parser = argparse.ArgumentParser()
-    #fmt:off
-    parser.add_argument( "-r", "--root", type=str, default="./data/03_replay_trajectory/d04-rec-01", 
-                         help="root dir") 
-    parser.add_argument( "-l", "--log", type=str, default="DEBUG", 
-                         help="log level") #fmt:on
-    args = parser.parse_args()
+    # ------------------------------------------------------------
+    # Setup
+    # ------------------------------------------------------------
 
     # Important paths
     log_level = args.log
@@ -144,24 +139,33 @@ def main():
     regex = ""
 
     # Robot points
-    robot_jp = root/"robot_mov"/"robot_jp_temp.txt"
+    robot_jp = root / "robot_mov" / "robot_jp_temp.txt"
     robot_jp = pd.read_csv(robot_jp)
-    pitch_robot = pitch_orig_in_robot(robot_jp) #df with columns = [step,px,py,pz]
+    pitch_robot = pitch_orig_in_robot(robot_jp)  # df with columns = [step,px,py,pz]
 
     # Tracker points
-    pitch_tracker = pitch_orig_in_tracker(root) #df with columns = [step area tx, ty,tz]
+    pitch_tracker = pitch_orig_in_tracker(root)  # df with columns = [step area tx, ty,tz]
 
-    #combine df using step as index
+    # combine df using step as index
     final_df = pd.merge(pitch_robot, pitch_tracker, on="step")
 
-    #Save df
+    # Save df
     dst_f = Path("scripts/01_calibration_exp/results")
-    dst_f = dst_f / (root.name+"_registration_data.txt")
+    dst_f = dst_f / (root.name + "_registration_data.txt")
     final_df.to_csv(dst_f, index=None)
 
     return None
 
+
+parser = argparse.ArgumentParser()
+# fmt:off
+parser.add_argument( "-r", "--root", type=str, default="./data/03_replay_trajectory/d04-rec-02", 
+                help="root dir") 
+parser.add_argument( "-l", "--log", type=str, default="DEBUG", 
+                help="log level") #fmt:on
+args = parser.parse_args()
+
 if __name__ == "__main__":
 
-    # main()
+    main()
     calculate_registration()
--- scripts/01_calibration_exp/03_trajectory_analysis.py
+++ scripts/01_calibration_exp/03_trajectory_analysis.py
@@ -22,54 +22,37 @@ import rospy
 from kincalib.utils.Logger import Logger
 from kincalib.utils.SavingUtilities import save_without_overwritting
 from kincalib.utils.RosbagUtils import RosbagUtils
-from kincalib.utils.ExperimentUtils import separate_markerandfiducial, calculate_midpoints 
-from kincalib.geometry import Line3D,Circle3D, Triangle3D
+from kincalib.utils.ExperimentUtils import separate_markerandfiducial, calculate_midpoints
+from kincalib.geometry import Line3D, Circle3D, Triangle3D
 
 np.set_printoptions(precision=4, suppress=True, sign=" ")
 
-def load_files(root:Path):
-    dict_files = defaultdict(dict) #Use step as keys. Each entry has a pitch and roll file.
-    for f in (root/"pitch_roll_mov").glob("*"):
-        step = int(re.findall("step[0-9]+", f.name)[0][4:]) #Not very reliable
-        if "pitch" in f.name :
-            dict_files[step]['pitch'] = pd.read_csv(f)
+
+def load_files(root: Path):
+    dict_files = defaultdict(dict)  # Use step as keys. Each entry has a pitch and roll file.
+    for f in (root / "pitch_roll_mov").glob("*"):
+        step = int(re.findall("step[0-9]+", f.name)[0][4:])  # Not very reliable
+        if "pitch" in f.name:
+            dict_files[step]["pitch"] = pd.read_csv(f)
         elif "roll" in f.name:
-            dict_files[step]['roll'] = pd.read_csv(f)
+            dict_files[step]["roll"] = pd.read_csv(f)
 
-    return dict_files 
+    return dict_files
 
-
-
-# def calculate_area(m1,m2,m3)->float:
-#     return 0.5*np.linalg.norm(np.cross(1000*(m1 -m3),1000*(m2-m3)))    
-
-# def calculate_triangle_sides(m1,m2,m3)->List[float]:
-#     s1 = 1000*np.linalg.norm(m1-m2)   
-#     s2 = 1000*np.linalg.norm(m1-m3)   
-#     s3 = 1000*np.linalg.norm(m2-m3)
-#     return np.array([s1,s2,s3])
 
 def create_histogram(data):
     fig, axes = plt.subplots(1, 1, sharey=True, tight_layout=True)
-    axes.hist(data, bins=50, range=(0,100), edgecolor='black', linewidth=1.2, density=False)
+    axes.hist(data, bins=50, edgecolor="black", linewidth=1.2, density=False)
+    # axes.hist(data, bins=50, range=(0, 100), edgecolor="black", linewidth=1.2, density=False)
     axes.grid()
     axes.set_xlabel("Triangle area mm^2")
     axes.set_ylabel("Frequency")
     axes.set_title(f"Pitch axis measurements. (N={data.shape[0]:02d})")
-    axes.set_xticks([i*5 for i in range(110//5)])
+    # axes.set_xticks([i * 5 for i in range(110 // 5)])
     plt.show()
 
+
 def main():
-    # ------------------------------------------------------------
-    # Setup 
-    # ------------------------------------------------------------
-    parser = argparse.ArgumentParser()
-    #fmt:off
-    parser.add_argument( "-r", "--root", type=str, default="./data/03_replay_trajectory/d04-rec-01", 
-                         help="root dir") 
-    parser.add_argument( "-l", "--log", type=str, default="DEBUG", 
-                         help="log level") #fmt:on
-    args = parser.parse_args()
 
     # Important paths
     log_level = args.log
@@ -83,13 +66,16 @@ def main():
     x = 0
     list_area = []
     for k in keys:
-        m1,m2,m3 = calculate_midpoints(dict_files[k]['roll'],dict_files[k]['pitch'])
-        triangle = Triangle3D([m1,m2,m3])
-        #Scale sides and area to milimiters
-        sides = triangle.calculate_sides(scale=1000) 
+        if len(list(dict_files[k].keys())) < 2:
+            log.warning(f"files for step {k} are not available")
+            continue
+        m1, m2, m3 = calculate_midpoints(dict_files[k]["roll"], dict_files[k]["pitch"])
+        triangle = Triangle3D([m1, m2, m3])
+        # Scale sides and area to milimiters
+        sides = triangle.calculate_sides(scale=1000)
         area = triangle.calculate_area(scale=1000)
-        # sides = calculate_triangle_sides(m1,m2,m3) 
-        # area = calculate_area(m1,m2,m3) 
+        # sides = calculate_triangle_sides(m1,m2,m3)
+        # area = calculate_area(m1,m2,m3)
         log.debug(f"Step {k} results")
         log.debug(f"triangle sides {sides} mm")
         log.debug(f"triangle area {area:0.4f} mm^2")
@@ -99,27 +85,32 @@ def main():
     # create_histogram(list_area)
     log.info(f"Mean area {list_area.mean():0.4f}")
     log.info(f"Std  area {list_area.std():0.4f}")
-    
-    f_path = Path(__file__).parent /"results"/root.name
+
+    f_path = Path(__file__).parent / "results" / root.name
     log.info(f_path)
     np.save(f_path, list_area)
 
-def plot_results():
-    parser = argparse.ArgumentParser()
-    #fmt:off
-    parser.add_argument( "-r", "--root", type=str, default="./data/03_replay_trajectory/d04-rec-01", 
-                         help="root dir") 
-    parser.add_argument( "-l", "--log", type=str, default="DEBUG", 
-                         help="log level") #fmt:on
-    args = parser.parse_args()
 
+def plot_results():
     root = Path(args.root)
-    f_path = Path(__file__).parent /"results"/root.name
-    f_path = f_path.with_suffix(".npy") 
+    f_path = Path(__file__).parent / "results" / root.name
+    f_path = f_path.with_suffix(".npy")
     results = np.load(f_path)
 
     create_histogram(results)
 
+
+# ------------------------------------------------------------
+# Setup
+# ------------------------------------------------------------
+parser = argparse.ArgumentParser()
+# fmt:off
+parser.add_argument( "-r", "--root", type=str, default="./data/03_replay_trajectory/d04-rec-02", 
+                        help="root dir") 
+parser.add_argument( "-l", "--log", type=str, default="DEBUG", 
+                        help="log level") #fmt:on
+args = parser.parse_args()
+
 if __name__ == "__main__":
-    main()
+    # main()
     plot_results()
