2526014babc7ca1acbff183970b42e0192e21ef8
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-03-23 14:23:42 -0400

    signed angle calculation test

---
+++ tests/geometry/test_signed_angle_calc.py
@@ -0,0 +1,38 @@
+"""
+Script to calculate the signed angle between two vectors.
+"""
+# Python imports
+from pathlib import Path
+import time
+import argparse
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from typing import List, Tuple, Set
+from rich.logging import RichHandler
+from rich.progress import track
+
+# ROS and DVRK imports
+import dvrk
+import rospy
+
+# kincalib module imports
+from kincalib.utils.Logger import Logger
+from kincalib.utils.SavingUtilities import save_without_overwritting
+from kincalib.utils.RosbagUtils import RosbagUtils
+
+np.set_printoptions(precision=4, suppress=True, sign=" ")
+
+
+def main():
+    # ------------------------------------------------------------
+    # Sample comment
+    # ------------------------------------------------------------
+    pass
+
+
+if __name__ == "__main__":
+
+    main()
