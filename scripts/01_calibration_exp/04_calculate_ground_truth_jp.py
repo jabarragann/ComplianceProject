1df4233a7c549a70f9a7ef28eb5f6f84ba4d7b3a
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-09-04 18:31:41 -0400

    Updated arguments in script 04_ground_truth

--- scripts/01_calibration_exp/04_calculate_ground_truth_jp.py
+++ scripts/01_calibration_exp/04_calculate_ground_truth_jp.py
@@ -22,37 +22,38 @@ def main(testid: int):
     # Setup
     # ------------------------------------------------------------
 
-    # Important paths
     root = Path(args.root)
-    marker_file = Path("./share/custom_marker_id_112.json")
+    if not root.exists():
+        log.error(f"root folder does not exists")
+        exit()
 
-    if args.test:
-        robot_jp_p = root / f"test_trajectories/{testid:02d}" / "robot_jp.txt"
-        robot_cp_p = root / f"test_trajectories/{testid:02d}" / "robot_cp.txt"
+    registration_p = root / "registration_results/registration_values.json"
+    if not registration_p.exists():
+        log.error(
+            f"registration_values.json does not exists. Add calibration file to {registration_p.parent}"
+        )
+        exit()
+
+    # Src paths
+    if args.testdata:  # Test trajectories
+        data_p = Path(args.datadir) if args.datadir is not None else root
+        robot_jp_p = data_p / f"test_trajectories/{testid:02d}" / "robot_jp.txt"
+        robot_cp_p = data_p / f"test_trajectories/{testid:02d}" / "robot_cp.txt"
+    else:  # Calibration points
+        data_p = root / "robot_mov"
+        robot_jp_p = data_p / "robot_jp.txt"
+        robot_cp_p = data_p / "robot_cp.txt"
+
+    # Dst paths
+    if args.testdata:
+        dst_p = data_p / f"test_trajectories/{testid:02d}" / "result"
     else:
-        robot_jp_p = root / "robot_mov" / "robot_jp.txt"
-        robot_cp_p = root / "robot_mov" / "robot_cp.txt"
+        dst_p = data_p / "result"
+    dst_p.mkdir(exist_ok=True)
 
-    # ------------------------------------------------------------
-    # Calculate tracker joint values
-    # ------------------------------------------------------------
-    if args.dstdir is None:
-        dst_p = robot_cp_p.parent
-        dst_p = dst_p / "result"
-        registration_data_path = root / "registration_results"
-        dst_p.mkdir(exist_ok=True)
-
-    else:
-        dst_p = Path(args.dstdir)
-        registration_data_path = dst_p / root.name / "registration_results"
-        dst_p = (
-            dst_p / root.name / robot_cp_p.parent.name / "result"
-        )  # e.g d04-rec-11-traj01/01/results/
-        dst_p.mkdir(parent=True, exist_ok=True)
-
-    log.info(f"Storing results in {dst_p}")
-    log.info(f"Loading registration .json from {registration_data_path}")
-    # input("press enter to start ")
+    log.info(f"Storing results in \n{dst_p}")
+    log.info(f"Loading registration .json from \n{registration_p}")
+    log.info(f"Loading data from \n{data_p}")
 
     if (dst_p / "robot_joints.txt").exists() and not args.reset:
         # ------------------------------------------------------------
@@ -63,14 +64,12 @@ def main(testid: int):
         opt_df = pd.read_csv(dst_p / "opt_error.txt", index_col=None)
     else:
         # ------------------------------------------------------------
-        # Calculate joints
+        # Calculate tracker joint values
         # ------------------------------------------------------------
         # Read data
         robot_jp = pd.read_csv(robot_jp_p)
         robot_cp = pd.read_csv(robot_cp_p)
-        registration_dict = json.load(
-            open(registration_data_path / "registration_values.json", "r")
-        )
+        registration_dict = json.load(open(registration_p, "r"))
         # calculate joints
         T_TR = Frame.init_from_matrix(np.array(registration_dict["robot2tracker_T"]))
         T_RT = T_TR.inv()
@@ -84,9 +83,6 @@ def main(testid: int):
             j_estimator, robot_jp, robot_cp
         )
 
-        # Save data
-        if not dst_p.exists():
-            dst_p.mkdir(parents=True)
         robot_df.to_csv(dst_p / "robot_joints.txt", index=False)
         tracker_df.to_csv(dst_p / "tracker_joints.txt", index=False)
         opt_df.to_csv(dst_p / "opt_error.txt", index=False)
@@ -102,10 +98,8 @@ def main(testid: int):
     table.add_data(robot_error_metrics.create_error_dict())
 
     print("")
-    print(
-        f"**Evaluation report for test trajectory {testid} in {registration_data_path.parent.name}**"
-    )
-    print(f"* Registration path: {registration_data_path}")
+    print(f"**Evaluation report for test trajectory {testid} in {registration_p.parent.name}**")
+    print(f"* Registration path: {registration_p}")
     print(
         f"Difference from ground truth (Tracker values) (N={robot_error_metrics.joint_error.shape[0]})"
     )
@@ -133,27 +127,30 @@ def main(testid: int):
 
 
 if __name__ == "__main__":
-    ## TODO: DSTDIR param should copy the result and original files. currently only copying the results.
 
     parser = argparse.ArgumentParser()
     # fmt:off
     parser.add_argument( "-r", "--root", type=str, default="./data/03_replay_trajectory/d04-rec-11-traj01", 
-                    help="root dir") 
-    parser.add_argument('-t','--test', action='store_true',help="Use test data")
-    # parser.add_argument("--testid", type=int, help="The id of the test trajectory to use") 
-    parser.add_argument('--testid', nargs='*', help='test trajectories to generate', required=True, type=int)
+                    help="root dir. This directory must contain a subdir `registration_results` with calibration .json file."
+                    "Test trajectories should be saved in a `save_trajectories` subdir.") 
+    parser.add_argument('-t','--testdata', action='store_true',help="Use test data")
+    # parser.add_argument("--testid", type=int, help="") 
+    parser.add_argument('--testid', nargs='*', help='List test ids to analyze', required=True, type=int)
     parser.add_argument( '-l', '--log', type=str, default="DEBUG", help="log level") 
     parser.add_argument('--reset', action='store_true',help="Recalculate joint values")
-    parser.add_argument('--dstdir', default=None, help='Directory to save results. This directory must ' \
-                            'contain a calibration .json file. If None, root dir is used a destination.')
+    parser.add_argument("--datadir", default = None, type=str, help="Use data in a different directory. Only" 
+                        "used with the --testdata option.")
     parser.add_argument('-p','--plot', action='store_true',default=False \
                         ,help="Plot optimization error and joints plots.")
     # fmt:on
     args = parser.parse_args()
     log_level = args.log
-    log = Logger("pitch_exp_analize2", log_level=log_level).log
+    log = Logger(__name__, log_level=log_level).log
 
     log.info(f"Calculating the following test trajectories: {args.testid}")
 
-    for testid in args.testid:
-        main(testid)
+    if args.testdata:
+        for testid in args.testid:
+            main(testid)
+    else:
+        main(0)
