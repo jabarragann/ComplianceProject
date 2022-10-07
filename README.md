ec4f4465f0dd473f2151747fc3b2856cec11ae3e
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-10-07 11:41:35 -0400

    install dependencies script

--- README.md
+++ README.md
@@ -24,7 +24,8 @@ After installing the module, open a python terminal and write
 import kincalib
 ```
 
-to ensure the module was correctly installed.
+to ensure the module was correctly installed. To facilitate the installation process the `install-dependencies.sh` is provided.
+
 
 ### Training of neural networks (Optional)
 To train the neural networks with the code provided you will need to additionally install the [Torch-suite](https://github.com/jabarragann/torch-suite) module and pytorch dependencies with
---
+++ install-dependencies.sh
@@ -0,0 +1,18 @@
+#Create python virtual env
+git checkout icra-2023 
+conda env create -f environment.yml 
+conda activate kincalibenv
+#Install kaggle api and download data
+pip install kaggle
+kaggle datasets download -d juanantoniobarragan/kinematic-calibration-of-surgical-robots
+unzip kinematic-calibration-of-surgical-robots.zip
+#Install deep learning dependencies
+pip install -r requirements_torch.txt 
+git clone https://github.com/jabarragann/torch-suite.git --recursive
+cd torch-suite
+git checkout ff68b04
+pip install -e .
+pip install -e ./pytorch-checkpoint/
+cd ..
+#Run script to calculate neural net corrected joints
+python3 scripts/01_calibration_exp/07_plot_corrected_joints.py -r ./icra2023-data/d04-rec-20-trajsoft/ --testid 3 7 26 -t -m ./icra2023-data/neuralnet/model
